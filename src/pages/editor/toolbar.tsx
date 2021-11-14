094c26ba2491bc3d06fde6f7acd17c29be2a467e
yujs <1062888640@qq.com>
2021-11-14 22:32:58 +0800

    add toolbar

---
+++ src/pages/editor/toolbar.tsx
@@ -0,0 +1,16 @@
+
+import {Button,Input,Select} from 'antd';
+
+const Toolbar = ()=>{
+
+    return  <div>
+        <Button></Button>
+        <Input></Input>
+        <Select>
+            <Select.Option value="111">111</Select.Option>
+        </Select>
+    </div>
+
+}
+
+export default Toolbar;
